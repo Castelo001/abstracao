017d51afc051e0d13a79fa425d5dbe95bdd436f2
Fernando J.r <castelobrancomoura@hotmail.com>
2024-02-24 12:05:52 -0300

    Primeiro commit

---
+++ src/abstracao/Principal.java
@@ -0,0 +1,10 @@
+package abstracao;
+import abstracao.dominio.Pessoa;
+import sobrecarga.util.EntradaConsole;
+public class Principal {
+
+	public static void main(String[] args) {
+		String nome = EntradaConsole.capturarTexto("Digite o Nome")
+	}
+
+}
---
+++ src/abstracao/dominio/Pessoa.java
@@ -0,0 +1,13 @@
+package abstracao.dominio;
+
+import java.security.PublicKey;
+
+public class Pessoa {
+
+public String nome;
+public int idade;
+public void exibirDados() {
+	System.out.println("Nome: " + nome +", Idade: " + idade);
+public static void main    
+}
+}
